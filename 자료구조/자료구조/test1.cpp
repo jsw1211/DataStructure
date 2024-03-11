0c8249796c12d5718923f71b93be2f3a8f3c89f7
jsw1211 <jswtiger@naver.com>
2024-03-11 14:06:32 +0900

    돌 하나 놓기 성공

--- 자료구조/자료구조/test1.cpp
+++ 자료구조/자료구조/test1.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <Windows.h>
 
+char board[19][19] = { };
+
 void PrintBoard() {
-	char board[19][19] = { };
+	
 	for (int i = 0; i < 19; ++i) {
 		for (int j = 0; j < 19; ++j) {
 			board[j][i] = '+';
@@ -10,14 +12,35 @@ void PrintBoard() {
 		}
 		printf("\n");
 	}
-
 }
 
+void ChangeBoard() {
+	for (int i = 0; i < 19; ++i) {
+		for (int j = 0; j < 19; ++j) {
+			printf("%c ", board[j][i]);
+		}
+		printf("\n");
+	}
+}
+
+void PutStone(int turn) {
+	turn = 0;
+	int i, j;
+	scanf("%d %d", &i, &j);
+	if (board[j][i] == '+') {
+		board[j][i] = 'O';
+	}
+	ChangeBoard();
+
+}
 
 int main() {
 	system("cls");
 	
+	int turn = 0;
+
 	PrintBoard();
+	PutStone(turn);
 
 	return 0;
 }
