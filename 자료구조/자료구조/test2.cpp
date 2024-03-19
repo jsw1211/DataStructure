128809bb59c65b44c6b29d1b9a367f492b3da49b
jsw1211 <jswtiger@naver.com>
2024-03-19 23:43:12 +0900

    test2 추가

---
+++ 자료구조/자료구조/test2.cpp
@@ -0,0 +1,19 @@
+//#include <stdio.h>
+//
+//struct Champion {
+//
+//	char name[10] = { NULL };
+//	int hp = NULL;
+//	int mp = NULL;
+//	int speed = NULL;
+//	int range = NULL;
+//	char position[10] = { NULL };
+//
+//};
+//
+//int main() {
+//
+//	struct Champion champion[50];
+//
+//	return 0;
+//}
